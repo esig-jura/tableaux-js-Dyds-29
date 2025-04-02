ec434264ef111858bf77e01b02d980e0031476a9
Dylan <dylan.texeirabatista@divcom.ch>
2025-04-02 14:25:15 +0200

    création tbx dans le js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,28 @@ const personnes = [
         prenom: 'Jim',
         age: 40,
         localite: 'San Francisco',
+    },
+    {
+        nom: 'Gasser',
+        prenom: 'Morgan',
+        age: 56,
+        localite: 'Miami',
     }
 ];
 
 const affichePersonne = document.querySelector('#affichePersonne');
+
+personnes.forEach(personne =>
+{
+    const tr = document.createElement('tr');
+
+    tr.innerHTML =
+        `
+    <td>${personne.prenom}</td>
+    <td>${personne.nom}</td>
+    <td>${personne.age}</td>
+    <td>${personne.localite}</td>
+`;
+
+    affichePersonne.appendChild(tr);
+});
